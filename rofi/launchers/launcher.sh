7cbcb90f4e6c1d652674deddaf83e6cd0d7b2b19
avs-odoo <avs@odoo.com>
2020-10-17 15:03:55 +0200

    [ADD] config: rofi

---
+++ rofi/launchers/launcher.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+## Author : Aditya Shakya (adi1090x)
+## Mail : adi1090x@gmail.com
+## Github : @adi1090x
+## Reddit : @adi1090x
+
+style="style_normal_grid"
+
+rofi -no-lazy-grab -show drun -modi window,drun,ssh -theme launchers/"$style".rasi
